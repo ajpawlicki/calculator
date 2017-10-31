222520e44af492022e722bbc7b6aaf64a20ebec6
AJ Pawlicki <aloysiuspaw@gmail.com>
2017-10-31 13:19:06 -0700

    Add calculate route

--- server/app.js
+++ server/app.js
@@ -3,11 +3,28 @@ const app = express();
 
 const bodyParser = require('body-parser');
 
-app.use(express.static(__dirname + '/../client/dist/'));
-app.use(bodyParser);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(__dirname + '/../client/'));
 
 app.post('/calculate', (req, res) => {
+  let {pre, post, operator} = req.body;
+  let ans;
   
+  pre = +pre;
+  post = +post;
+
+  if (operator === '+') {
+    ans = pre + post;
+  } else if (operator === '-') {
+    ans = pre - post;
+  } else if (operator === '*') {
+    ans = pre * post;
+  } else if (operator === '/') {
+    ans = pre / post;
+  }
+
+  res.send(JSON.stringify( ans ));
 });
 
 app.listen(5000, () => console.log('Listening on port 5000!'));
