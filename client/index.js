18ad723375af05499196143ef4ce43d0eaf871b0
AJ Pawlicki <aloysiuspaw@gmail.com>
2017-10-31 13:19:27 -0700

    Add post req on client

--- client/index.js
+++ client/index.js
@@ -1,3 +1,27 @@
 window.onload = () => {
+  const form = document.getElementById('calc-form');
+  const pre = document.getElementById('pre');
+  const post = document.getElementById('post');
+  const operator = document.getElementById('operator');
+  const result = document.getElementById('result');
 
+  form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    if (pre.value.length && post.value.length) {
+      const data = {
+        pre: pre.value,
+        post: post.value,
+        operator: operator.value
+      };
+      
+      $.post('/calculate', data, (ans) => {
+        result.innerHTML = '';
+        result.innerHTML = ans;
+      });
+
+    } else {
+      result.innerHTML = 'Please provide two valid numbers';
+    }
+  });
 };
